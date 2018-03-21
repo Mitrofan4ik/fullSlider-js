ec1a31564c4f77defe0f3962209a5b3d70c5d04e
NIX\mytrofanova <ekaterinamytrofanova@nixsolutions.com>
2018-03-21 12:21:32 +0200

    compute translatex

--- js/slider5.js
+++ js/slider5.js
@@ -10,27 +10,28 @@ function Slider(settings) {
 
   var clickCounter = 0; // первое значение счетчика
 
+    
   var init = function () {
-    for (var i = 0; i < sliderItems.length; i++) {
-       singleSliderItemsWidth = parseFloat(getComputedStyle(sliderItems[i]).width);
-       console.log(singleSliderItemsWidth); // ширина в пикселах одного слайда в слайдере
-      }
-      console.log(i); 
+  for (var i = 0; i < sliderItems.length; i++) {
+      singleSliderItemsWidth = parseFloat(getComputedStyle(sliderItems[i]).width);
+      console.log(singleSliderItemsWidth); // ширина в пикселах одного слайда в слайдере
+    }
+    console.log(i); 
+  
     
-      
-      var hiddenSlides = i - slidesNumberVisible;
-      
-      
-    } 
-    init();
+    var hiddenSlides = i - slidesNumberVisible;
     
-    var sliderWidthContainer =  slider.clientWidth; // ширина контейнера родителя 
-    var slidesNumberVisible = Math.round(sliderWidthContainer / singleSliderItemsWidth); // количество слайдов которое помещаеться в эран (в контейнер родителя)
-    var hiddenSlides = sliderItems.length - slidesNumberVisible; // количество слайдов которое НЕ помещаеться в эран (в контейнер родителя)
     
-    console.log(sliderWidthContainer + "clientWidth");
-    console.log(slidesNumberVisible + " окрулила до ближайшего целого");
-    console.log(hiddenSlides + " количество слайдов за пределами окна");
+  } 
+  init();
+    
+  var sliderWidthContainer =  slider.clientWidth; // ширина контейнера родителя 
+  var slidesNumberVisible = Math.round(sliderWidthContainer / singleSliderItemsWidth); // количество слайдов которое помещаеться в эран (в контейнер родителя)
+  var hiddenSlides = sliderItems.length - slidesNumberVisible; // количество слайдов которое НЕ помещаеться в эран (в контейнер родителя)
+  
+  console.log(sliderWidthContainer + "clientWidth");
+  console.log(slidesNumberVisible + " окрулила до ближайшего целого");
+  console.log(hiddenSlides + " количество слайдов за пределами окна");
   
   // sliderGallery.addEventListener('click', function(e) {
   //   console.dir(e.target)
@@ -56,24 +57,48 @@ function Slider(settings) {
   sliderGallery.addEventListener('click', function(e) {
     if (e.target.classList.contains('prev')) {
       clickCounter += 1;
-
+      
       console.log(clickCounter)
       
       sliderContent.style.transform = 'translateX(' + (clickCounter * singleSliderItemsWidth) + 'px)';
-    } else if (clickCounter == hiddenSlides) {
-      e.target.classList.add("disable");
-    }
+      
+      setTimeout(getTranslateX, 1000);
+    } 
+    // else if (clickCounter == hiddenSlides) {
+    //   e.target.classList.add("disable");
+    // }
   });
 
   sliderGallery.addEventListener('click', function(e) {
     if (e.target.classList.contains('next')) {
       clickCounter -= 1;
-
+      
       console.log(clickCounter)
-
+      
       sliderContent.style.transform = 'translateX(' + (clickCounter * singleSliderItemsWidth) + 'px)';
-    } else if (clickCounter == hiddenSlides) {
-      e.target.classList.add("disable");
+      
+      setTimeout(getTranslateX, 1000);
     }
+    //  else if (clickCounter == hiddenSlides) {
+    //   e.target.classList.add("disable");
+    // }
   });
 };
+
+
+var myElement = document.getElementById("sliderContent");
+// myElement.style.transform = 'translateX( "singleSliderItemsWidth" + "px")';
+
+function getTranslateX() {
+  var style = window.getComputedStyle(myElement);
+  var matrix = new WebKitCSSMatrix(style.webkitTransform);
+  console.log(matrix);
+  console.log('translateX: ', matrix.m41);
+}
+getTranslateX();
+
+// window.addEventListener('load', function(){
+//   var curTransform = new WebKitCSSMatrix(window.getComputedStyle(transformed).webkitTransform);
+//   var sliderPositionHelper = document.getElementById(sliderContent).curTransform;
+//   console.log(sliderPositionHelper);
+// });
